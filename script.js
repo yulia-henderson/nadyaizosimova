001306c87ed647ccd1b0b5cad5d77d0920e0708c
yulia-henderson <yuliahenderson@icloud.com>
2019-10-21 15:32:26 -0400

    changed the width of the sidenav

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
   }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
